99fc6fdf4c2d8b7ae87729b35170153f56b6562c
Aml Abdelrhman <Aml586651@gmail.com>
2026-08-13 03:51:40 +0300

    Add providers.php configuration file

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -4,4 +4,7 @@ use App\Providers\AppServiceProvider;
 
 return [
     AppServiceProvider::class,
+    \Illuminate\View\ViewServiceProvider::class,
+    \Illuminate\Session\SessionServiceProvider::class,
+    \Illuminate\Cookie\CookieServiceProvider::class,
 ];
